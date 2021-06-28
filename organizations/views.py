a9bbabd6a4ea8afb381811435963ff5610f56480
Savva <kwaket@live.ru>
2021-06-28 17:17:10 +0300

    add pagination

--- organizations/views.py
+++ organizations/views.py
@@ -1,9 +1,13 @@
 import organizations
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Organization
 
 
 def organization_list(request):
-    organizations_list = Organization.objects.all().order_by('-id')[:100]
+    org_list = Organization.objects.all()
+    paginator = Paginator(org_list, 25)
+    page = request.GET.get('page')
+    orgs = paginator.get_page(page)
     return render(request, 'organizations/organization_list.html',
-        {'organizations': organizations_list})
+        {'organizations': orgs})
