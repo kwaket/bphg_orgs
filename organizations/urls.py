2d455e5920432f234bc835462a3c39e6390a2e2e
Savva <kwaket@live.ru>
2021-06-30 01:11:19 +0300

    edit urls

--- organizations/urls.py
+++ organizations/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-import organizations
 
 
 urlpatterns = [
@@ -10,7 +9,7 @@ urlpatterns = [
     path('organizations/new/', views.organization_new, name='organization_new'),
     path('organizations/<int:pk>/edit', views.organization_edit, name='organization_edit'),
     path('projects', views.project_list, name='project_list'),
-    path('projects/<int:pk>/', views.organization_detail, name='project_detail'),
-    path('projects/new/', views.organization_new, name='project_new'),
-    path('projects/<int:pk>/edit', views.organization_edit, name='project_edit'),
+    path('projects/<int:pk>/', views.project_detail, name='project_detail'),
+    path('projects/new/', views.project_new, name='project_new'),
+    path('projects/<int:pk>/edit', views.project_edit, name='project_edit'),
 ]
