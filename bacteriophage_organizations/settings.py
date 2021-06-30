34d1c1a7c06006ae6abd79cfc2111b6f144b7ed2
Savva <kwaket@live.ru>
2021-06-30 23:42:44 +0300

    add crispy from

--- bacteriophage_organizations/settings.py
+++ bacteriophage_organizations/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_bootstrap5',
     'django_filters',
+    'crispy_forms',
 
     'organizations',
 ]
@@ -133,3 +134,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
