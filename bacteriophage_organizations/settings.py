df487f4b2798212cff80c85bfb6c40f5df7d681f
Savva <kwaket@live.ru>
2021-07-08 16:13:42 +0300

    edit settings

--- bacteriophage_organizations/settings.py
+++ bacteriophage_organizations/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-np*nq6pstmacrbx%gp$y=_wfd^k6fymep32i91db&&dd18%jkn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', os.environ.get('APP_HOST')]
 
 
 # Application definition
