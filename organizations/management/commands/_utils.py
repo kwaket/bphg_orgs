5dc9879e84ad742fc5cc171a6d1285dd03997e98
savva <+s.g.fillipov@microgen.ru>
2021-09-07 14:30:19 +0300

    add getting image from site

--- organizations/management/commands/_utils.py
+++ organizations/management/commands/_utils.py
@@ -1,3 +1,5 @@
+import re
+from urllib.parse import urljoin
 from io import BytesIO
 from typing import Union
 import random
@@ -8,9 +10,27 @@ import numpy as np
 from PIL import Image
 
 
+def _filter_by_ext(l:list, ext:str) -> list:
+    return list(filter(lambda x: not x.lower().endswith(ext), l))
+
+
+def get_imageurl_from_site(url:str) -> str:
+    r = requests.get(url)
+    imgs = re.findall(r'<img.*?>', str(r.content))
+    imgs = [re.findall(r'src\s?=\s?["\']{1}(.*?)["\']{1}\s', i) for i in imgs]
+    imgs = [_filter_by_ext(i, '.svg') for i in imgs]
+    imgs = [urljoin(url, i[0]) for i in imgs if i]
+    imgs = [i for i in imgs if not i[0].lower().endswith('.svg')]
+    return imgs[0]
+
 
 def get_file_by_url(url:str) -> Union[BytesIO, None]:
-    r = requests.get(url)
+    print(url)
+    try:
+        r = requests.get(url)
+    except requests.exceptions.MissingSchema:
+        return None
+
     if r.status_code == 200:
         return BytesIO(r.content)
     return None
@@ -31,4 +51,5 @@ def get_theme_by_urlfile(url:str) -> str:
 
 
 def is_dark_theme(url: str) -> bool:
+    print(url)
     return get_theme_by_urlfile(url) == 'dark'
--- organizations/management/commands/update_organization.py
+++ organizations/management/commands/update_organization.py
@@ -2,7 +2,7 @@ import random
 from django.core.management.base import BaseCommand, CommandError
 from organizations.models import Organization
 
-from ._utils import is_dark_theme
+from ._utils import is_dark_theme, get_imageurl_from_site
 
 class Command(BaseCommand):
     help = 'Update organization card theme'
@@ -10,6 +10,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         orgs = Organization.objects.all()
         for org in orgs:
+            if not org.image:
+                org.image = get_imageurl_from_site(org.site)
             if org.image:
                 org.theme_is_dark = is_dark_theme(org.image)
             elif not org.image and org.theme_is_dark is not None:
