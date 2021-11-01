b76e2f4f001352f3f79180c57a541c0aaa9a1159
savva <+s.g.fillipov@microgen.ru>
2021-11-01 15:02:16 +0300

    add download images and logos to media directory

--- organizations/management/commands/_utils.py
+++ organizations/management/commands/_utils.py
@@ -1,14 +1,26 @@
 import re
-from urllib.parse import urljoin
+import os
 from io import BytesIO
 from typing import Union
-import random
+from urllib.parse import urlparse, urljoin
+import datetime as dt
+
+from django.conf import settings
 
 import requests
-# from requests.models import requote_uri
 import numpy as np
 from PIL import Image
-from requests import exceptions
+from requests.exceptions import ConnectionError
+
+
+def _set_cipher_set(reqs):
+    reqs.packages.urllib3.disable_warnings()
+    reqs.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
+    try:
+        reqs.packages.urllib3.contrib.pyopenssl.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
+    except AttributeError:
+        pass
+    return reqs
 
 
 def _filter_by_ext(l:list, ext:str) -> list:
@@ -22,8 +34,11 @@ def _get_image_proportion(img: Image, top_limit=1) -> int:
     return r
 
 
-def get_imageurl_from_site(url:str) -> str:
-    r = requests.get(url)
+def get_imageurl_from_site(url:str) -> Union[str, None]:
+    try:
+        r = requests.get(url)
+    except ConnectionError:
+        return None
     imgs = re.findall(r'<img.*?>', str(r.content.decode('utf-8')))
     imgs = [re.findall(r'src\s?=\s?["\']{1}(.*?)["\']{1}\s', i) for i in imgs]
     imgs = [item for sublist in imgs for item in sublist]
@@ -46,17 +61,42 @@ def get_imageurl_from_site(url:str) -> str:
     imgs = [(url, _get_image_proportion(Image.open(b)) , Image.open(b).size[0] * Image.open(b).size[1]) for (url, b) in imgs]
 
     imgs.sort(key=lambda x: -x[1])
-    imgs = [i[0] for i in imgs]
+    imgs = [i[0] for i in imgs if not i[0].lower().endswith('.svg')]
+    imgs = [i for i in imgs if len(i) < 2048]
     return imgs[0] if imgs else None
 
 
+# def download_image_from_site(url: str) -> Union[str, None]:
+#     img_url = get_imageurl_from_site(url)
+#     if not img_url:
+#         return None
+#     filepath = urlparse(url).path
+#     filename = dt.datetime.now().strftime('%d.%m.%Y %H%M%S%f') + filepath
+#     try:
+#         req = requests.get(img_url)
+#     except (MissingSchema, RequestException, ConnectionError):
+#         return None
+#     else:
+#         with open(os.path.join(settings.MEDIA_ROOT, filename), 'wb') as fw:
+#             fw.write(req.content)
+#     return urljoin(settings.MEDIA_URL, filepath)
+
+
+def download_file_to_media(url: str) -> str:
+    filepath = urlparse(url).path
+    filename = os.path.split(filepath)[1]
+    reqs = _set_cipher_set(requests) 
+    req = reqs.get(url, verify=False)
+    filename = dt.datetime.now().strftime('%d.%m.%Y %H%M%S%f') + filename.replace('%', '')
+    with open(os.path.join(settings.MEDIA_ROOT, filename), 'wb') as fw:
+        fw.write(req.content)
+    return urljoin(settings.MEDIA_URL, filename)
+
+
 def get_file_by_url(url:str) -> Union[BytesIO, None]:
-    print(url)
     try:
         r = requests.get(url)
-    except requests.exceptions.MissingSchema:
-        return None
-    except requests.exceptions.RequestException:
+    except (requests.exceptions.MissingSchema, requests.exceptions.RequestException):
         return None
     if r.status_code == 200:
         return BytesIO(r.content)
@@ -68,9 +108,9 @@ def img_estim(img, thrshld):
     return 'light' if is_light else 'dark'
 
 
-def get_theme_by_urlfile(url:str) -> str:
+def get_theme_by_urlfile(url:str) -> Union[str, None]:
     fobj = get_file_by_url(url)
-    if fobj:
+    if fobj and not url.lower().endswith('.svg'):
         f = Image.open(fobj)
         return img_estim(f, 127)
     else:
--- organizations/management/commands/update_organization.py
+++ organizations/management/commands/update_organization.py
@@ -2,7 +2,7 @@ import random
 from django.core.management.base import BaseCommand, CommandError
 from organizations.models import Organization
 
-from ._utils import is_dark_theme, get_imageurl_from_site
+from ._utils import download_file_to_media
 
 class Command(BaseCommand):
     help = 'Update organization card theme'
@@ -10,13 +10,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         orgs = Organization.objects.all()
         for org in orgs:
-            if not org.image:
-                org.image = get_imageurl_from_site(org.site)
-            if org.image:
-                org.theme_is_dark = is_dark_theme(org.image)
-            elif not org.image and org.theme_is_dark is not None:
-                continue
-            else:
-                org.theme_is_dark = bool(random.randint(0, 1))
-            org.save()
-
+            if org.image and not org.image.startswith('/'):
+                org.image = download_file_to_media(org.image)
+                org.save()
+            if org.logo and not org.logo.startswith('/'):
+                org.logo = download_file_to_media(org.logo)
+                org.save()
