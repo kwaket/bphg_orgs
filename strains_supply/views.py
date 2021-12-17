d78e9ea3c4a7da35540352196918d455c0bde109
Savva <kwaket@live.ru>
2021-12-17 08:29:48 +0300

    rewrite main page

--- strains_supply/views.py
+++ strains_supply/views.py
@@ -4,6 +4,11 @@ from .models import Supply
 from .forms import SupplyForm
 
 
+def main_page(request):
+    supply_list = Supply.objects.all()
+    return render(request, 'strains_supply/main.html',
+                  {'supply_list': supply_list})
+
 def new_supply(request):
     form = SupplyForm()
     return render(request, 'strains_supply/edit_supply.html', {'form': form})
