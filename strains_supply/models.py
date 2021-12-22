a69fd33bdfac57e73cec5d44767a2fc0ebedd400
Savva <kwaket@live.ru>
2021-12-22 12:02:07 +0300

    edit model

--- strains_supply/models.py
+++ strains_supply/models.py
@@ -70,9 +70,9 @@ class Supply(UpdatingMixin):
                                on_delete=models.PROTECT)
     dest = models.ForeignKey(CompanyBranch, verbose_name='Филиал',
                              on_delete=models.PROTECT)
-    sent_at = models.DateField(null=True, blank=True,
+    suggested_sent_date = models.DateField(null=True, blank=True,
                                    verbose_name='Предполагаемая дата отправки')
-    num = models.IntegerField(null=True, blank=True,
+    suggested_num = models.IntegerField(null=True, blank=True,
                                verbose_name='Предполагаемое количество')
     recieved_at = models.DateTimeField(null=True, blank=True,
                                        verbose_name='Дата приема')
