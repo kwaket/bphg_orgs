06892a235605f0384c0f7f0eb021ec9f191a8661
Savva <kwaket@live.ru>
2022-02-09 20:21:57 +0300

    edit local environment

--- bacteriophage_organizations/settings/local.py
+++ bacteriophage_organizations/settings/local.py
@@ -22,6 +22,11 @@ CACHES = {
     }
 }
 
+# WhiteNoise
+# ------------------------------------------------------------------------------
+# http://whitenoise.evans.io/en/latest/django.html#using-whitenoise-in-development
+INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS  # noqa F405
+
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
@@ -40,6 +45,9 @@ if env("USE_DOCKER") == "yes":
 
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
+
+    DATABASES = {"default": env.db("DATABASE_URL")}
+    DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 # django-extensions
 # ------------------------------------------------------------------------------
